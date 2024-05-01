474d2bfc05a84d5330ecbeefc7aa896cfd88b783
Sophia Otis <sophianotis3@gmail.com>
2024-04-30 19:59:11 -0600

    done

---
+++ data/hoteldata.js
@@ -0,0 +1,50 @@
+export const hotels = [
+  {
+    "name":"California",
+    "photo":"img1.webp",
+    "address":["2200 Pacific Coast Highway", "Los Angeles, CA"],
+    "phone":"1-307-886-1234"
+  },
+  {
+    "name":"Oregon",
+    "photo":"img2.webp",
+    "address":["710 Evergreen Terrace", "Portland, OR"],
+    "phone":"1-543-865-7657"
+  },
+  {
+    "name":"New York",
+    "photo":"img3.webp",
+    "address":["350 Fifth Avenue", "New York, NY"],
+    "phone":"1-446-976-4578"
+  },
+  {
+    "name":"London",
+    "photo":"img4.webp",
+    "address":["12 Baker Street", "London, UK"],
+    "phone":"1-469-369-5675"
+  },
+  {
+    "name":"Hawaii",
+    "photo":"img5.webp",
+    "address":[" 2550 Kalakaua Avenue", "Honolulu, HI"],
+    "phone":"1-307-578-2469"
+  },
+  {
+    "name":"Denmark",
+    "photo":"img6.webp",
+    "address":["18 Strandgade", "Copenhagen, DK"],
+    "phone":"1-307-865-3467"
+  },
+  {
+    "name":"Paris",
+    "photo":"img7.webp",
+    "address":["5 Rue de Rivoli", "Paris, FR"],
+    "phone":"1-090-765-5789"
+  },
+  {
+    "name":"Japan",
+    "photo":"img8.webp",
+    "address":["2-4-1 Nishishinjuku", "Tokyo, JP"],
+    "phone":"1-307-765-8765"
+  },
+]
---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+// inject current year in footer
+const numberOne = new Date();
+document.querySelector("#year").textContent = numberOne.getFullYear();
+
+//off canvas nav
+const hb = document.querySelector('#hamburgerBtn');
+const pw = document.querySelector('#pageWrapper');
+hb.addEventListener('click', () => {
+  pw.classList.toggle('moveOver');
+});
+
+
+//Grab hotel location data
+import {hotels} from '../data/hoteldata.js';
+//console.log(hotels);
+const hotelCards=document.querySelector('#cards')
+
+hotels.forEach(hotel => {
+  const mySection = document.createElement("section")
+
+  const theImage = document.createElement("img")
+  theImage.src = `images/${hotel.photo}`
+  theImage.alt = hotel.name
+
+  const theName = document.createElement("h2")
+  theName.textContent = hotel.name
+
+  const theAddress = document.createElement("address");
+  theAddress.innerHTML = `${hotel.address[0]}<br>${hotel.address[1]}`;
+
+  const thePhone = document.createElement("a");
+  thePhone.textContent = hotel.phone
+  thePhone.href = `tel:${hotel.phone}`
+
+  mySection.appendChild(theImage)
+  mySection.appendChild(theName)
+  mySection.appendChild(theAddress)
+  mySection.appendChild(thePhone)
+  hotelCards.appendChild(mySection)
+});
